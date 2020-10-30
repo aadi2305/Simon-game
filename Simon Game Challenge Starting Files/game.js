a46a890c4f76af410d97fd0335e3cc58b63a03bc
aadi2305 <57494876+aadi2305@users.noreply.github.com>
2020-10-30 14:16:22 +0530

    All files are uploaded

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,61 @@
+
+var buttonColours = ["red", "blue","green" ,"yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var count = 0, level = 1;
+
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomColor = buttonColours[randomNumber];
+    gamePattern.push(randomColor);
+    clickAnimation(randomColor);
+}
+
+
+
+$(document).keypress(function(){
+$("h1").text("Level: "+ level);
+nextSequence();
+})
+    
+
+
+$(".btn").click(function(){
+    var userClikcedButton = $(this).attr("id");
+    clickAnimation(userClikcedButton);
+    userClickedPattern.push(userClikcedButton);
+    count++;
+    if(count == level)checkAnswer();
+})
+
+
+function checkAnswer(){
+    for (var i =0 ; i< gamePattern.length;  i++) {
+        if(gamePattern[i]!=userClickedPattern[i]){
+            gameOver();
+            return
+        }
+    }
+    userClickedPattern = [];
+    count = 0;
+    level++;
+    $("h1").text("Level: "+ level);
+    setTimeout(function(){
+        nextSequence()
+    }, 700);
+    
+}
+function clickAnimation(color){
+    $("#"+color).addClass("pressed");
+    setTimeout(function(){
+        $("#"+color).removeClass("pressed"); 
+    }, 200);
+}
+
+function gameOver(){
+    $("h1").text("Game Over. Press any key to start again");
+    gamePattern = [];
+    userClickedPattern = [];
+    count = 0, level = 1;
+}
